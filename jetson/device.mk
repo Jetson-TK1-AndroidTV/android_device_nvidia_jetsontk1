87ee670d3ecce12febe2ad382b3bca5618839af2
Matt Gorski <matt.gorski@gmail.com>
2016-02-12 13:56:22 -0500

    Update device.mk
    fixed media_codecs

--- jetson/device.mk
+++ jetson/device.mk
@@ -70,10 +70,10 @@ PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4
 # Codec Configs
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/media/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
